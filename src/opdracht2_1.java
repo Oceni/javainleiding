c05ecde11a271a6dc00f193827eaff0d4ea699f6
xavier <xlandveld@roc-dev.com>
2016-09-08 17:59:07 +0200

    new color

--- src/opdracht2_1.java
+++ src/opdracht2_1.java
@@ -6,10 +6,10 @@ public class opdracht2_1 extends Applet {
 
 
     public void init() {
-        setBackground(Color.yellow);
+        setBackground(Color.blue);
     }
     public void paint(Graphics g){
-        g.setColor(Color.blue);
+        g.setColor(Color.yellow);
         g.drawString("Xavier", 50, 60);
         g.setColor(Color.red);
         g.drawString("Landveld", 60, 70);
---
+++ src/opdracht2_2.java
@@ -0,0 +1,18 @@
+import java.applet.Applet;
+import java. awt.*;
+
+
+public class opdracht2_2 extends Applet{
+
+    public void init(){ setBackground(Color.white);
+
+    }
+
+    public void paint(Graphics g){
+        g.setColor(Color.blue);
+        g.drawString("Xavier", 60,60);
+        g.setColor(Color.red);
+        g.drawString("Landveld",50,70);
+
+    }
+}
